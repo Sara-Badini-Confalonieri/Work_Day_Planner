5d00ab40e51021eb3b4afcd0eaa8cbea6f57f131
Sara Badini Confalonieri <sara.badiniconfalonieri@gmail.com>
2023-12-12 10:50:28 +0000

    created a for loop in js file to go through the hours

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-$(document).ready(function() {
+$(document).ready(function () {
   var todayDate = dayjs();
   $("#currentDay").text(todayDate.format('dddd, MMMM Do'));
-});
+
+  const localStorageData = getLocalStorageData();
+
+  const todayDataIndex = localStorageData.findIndex(function (element) {
+    return element.date === todayDate.format('MM-DD-YYYY');
+  });
+
+  if (todayDataIndex >= 0) {
+    const todayData = localStorageData[todayDataIndex];
+
+    for (let i = 9; i <= 17; i++) {
+      $('#time-block-' + i).val(todayData.data[i]);
+    }
+  }
+})
